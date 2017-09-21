ff78f66f16a23aaae41bf4ee263dce2b37eb2149
drilla <drilla@mail.ru>
2017-09-21 20:39:01 +0300

    init

---
+++ php/progression/Progression.php
@@ -0,0 +1,12 @@
+<?php
+
+
+abstract class Progression {
+    public static function isStringFormatValid(string $string) : bool {
+
+    }
+
+    public static function isProgression(string $string) : bool {
+
+    }
+}
---
+++ php/progression/run.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Все делаем просто, без всяких неймспейсов
+ */
+include "Progression.php";
+/**
+ */
+
+try {
+    if (isset($argv[1])) {
+        $testingString = $argv[1];
+    } else {
+        throw new Exception('String argument required.');
+    }
+
+    if (!Progression::isStringFormatValid($testingString)) {
+        throw new  Exception('Invalid string format.');
+    }
+
+    if (Progression::isProgression($testingString)) {
+        echo 'String is a progression!';
+    } else {
+        echo 'String is NOT a progression!';
+    }
+} catch (Throwable $error) {
+    echo $error->getMessage();
+} finally {
+    echo '\n';
+    return 1;
+}
+
+
+
+?>
+
+
